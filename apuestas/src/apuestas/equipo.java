de2b554d5db66c758ba6ad7a9d7faab94ce99b7a
Antonio carrbonell <acmtibi@gmail.com>
2014-03-10 16:25:51 -0300

    Realizando cambios

---
+++ apuestas/src/apuestas/equipo.java
@@ -0,0 +1,19 @@
+package apuestas;
+
+import javax.swing.JPanel;
+
+public class equipo extends JPanel {
+private String nombreequipo;
+private int golesfavor;
+private int golenEncontra;
+private int partidosGanados;
+private int partidosPerdidos;
+
+	/**
+	 * Create the panel.
+	 */
+	public equipo() {
+
+	}
+
+}
--- apuestas/src/apuestas/principal.java
+++ apuestas/src/apuestas/principal.java
@@ -4,7 +4,7 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		equipo equip=new equipo();
 	}
 
 }
